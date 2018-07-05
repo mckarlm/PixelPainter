bf39ca866c3e0787bb679061a6eeb0756646b99f
Mckarl Manuel <manmckarl@gmail.com>
2018-07-05 09:47:45 -1000

    basic cell coloring functioning

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,17 +1,31 @@
 var pixelPainter = (function (width, height){
   var grid = document.getElementById('pixelPainter');
 
+  let paint = 'black';
+  
+
+
+  function setColor(event){
+    const cell = event.target;
+    cell.style.backgroundColor = paint;
+  }
+
+  //Makes the grid//
+  //can't be adjusted user-side yet//
   for (let i=0; i<width; i++){
     for (let j=0; j<height; j++){
       var canvas = document.createElement('div');
       canvas.className = 'cell'
+      canvas.addEventListener('click', setColor);
+      grid.appendChild(canvas);
+     
       /*var canvasCell;
 
       canvasCell = document.createElement('div');
       canvasCell.className = 'cell';
-      grid.appendChild(canvasCell);*/
-      grid.appendChild(canvas);
-      /*canvas.style.height = height * 10
+      grid.appendChild(canvasCell);
+    
+      canvas.style.height = height * 10
       canvas.style.width = width * 10
       grid.style.height = canvas.style.height * 10
       grid.style.width = canvas.style.width * 10*/
@@ -19,3 +33,5 @@ var pixelPainter = (function (width, height){
   }
 
 })(10, 10)
+
+//media query//
