fc17a0d04e259d136ce03501f23bb50fd0008b01
Mckarl Manuel <manmckarl@gmail.com>
2018-07-05 11:06:13 -1000

    separated canvas from palette, broke the grid though

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,37 +1,56 @@
 var pixelPainter = (function (width, height){
   var grid = document.getElementById('pixelPainter');
+  var canvasDiv = document.createElement('div');
+  var paletteDiv = document.createElement('div');
 
-  let paint = 'black';
-  
-
+  let currentColor = 'black';
 
   function setColor(event){
     const cell = event.target;
-    cell.style.backgroundColor = paint;
+    cell.style.backgroundColor = currentColor;
+  };
+
+  canvasDiv.className = 'canvasDiv';
+  grid.appendChild(canvasDiv);
+
+  paletteDiv.className = 'paletteDiv';
+  grid.appendChild(paletteDiv);
+  
+  let paintHeight = 20;
+  let paintWidth = 20;
+  
+  function makePalette(){
+    for (let k=0; k<paintHeight; i++){
+      for (let l=0; l<paintWidth; i++){
+        let paintCell = document.createElement('div');
+        paintCell.className = 'changeColor'
+        paletteDiv.appendChild(paintCell);
+      }
+    }
   }
+  makePalette();
+
+/*
+  for (let k=0; k<5; k++){
+    var makePalette = document.createElement('div');
+    makePalette.className = 'palette';
+    paletteDiv.appendChild(makePalette);
+  };
+*/
 
   //Makes the grid//
   //can't be adjusted user-side yet//
   for (let i=0; i<width; i++){
     for (let j=0; j<height; j++){
-      var canvas = document.createElement('div');
-      canvas.className = 'cell'
-      canvas.addEventListener('click', setColor);
-      grid.appendChild(canvas);
-     
-      /*var canvasCell;
+      var makeGrid = document.createElement('div');
+      //var makePalette = document.createElement('div');
 
-      canvasCell = document.createElement('div');
-      canvasCell.className = 'cell';
-      grid.appendChild(canvasCell);
-    
-      canvas.style.height = height * 10
-      canvas.style.width = width * 10
-      grid.style.height = canvas.style.height * 10
-      grid.style.width = canvas.style.width * 10*/
+      makeGrid.className = 'canvas';
+      makeGrid.addEventListener('click', setColor);
+      canvasDiv.appendChild(makeGrid);
     }
   }
 
 })(10, 10)
 
-//media query//
+//media queries//
